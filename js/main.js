db5462692e2d0c823cfa7f772ed66df8c714dda1
MD Hridoy Hossain <HridoyMrhn@github.com>
2020-11-24 21:06:24 +0600

    Work Section
    Work Section Completed

--- js/main.js
+++ js/main.js
@@ -1,8 +1,13 @@
+// For Slider
 var myCarousel = document.querySelector('#our-slider')
 var carousel = new bootstrap.Carousel(myCarousel, {
   interval: 2000,
   wrap: true,
   slide: true,
 //   ride: true
-})
+});
+
+// Mixitup - For Filtering
+var containerEl = document.querySelector('.gallery');
+var mixer = mixitup(containerEl);
 
