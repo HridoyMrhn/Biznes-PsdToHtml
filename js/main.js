1691a6ed8d60644c5ed1c9f0acb63279cbdfbb1c
MD Hridoy Hossain <HridoyMrhn@github.com>
2020-11-28 03:35:27 +0600

    Full Design
    Full Design Completed

--- js/main.js
+++ js/main.js
@@ -1,18 +1,40 @@
 // For Slider
 var myCarousel = document.querySelector('#our-slider')
 var carousel = new bootstrap.Carousel(myCarousel, {
-  interval: 2000,
-  wrap: true,
-  slide: true,
-//   ride: true
+    interval: 2000,
+    wrap: true,
+    slide: true,
+    //   ride: true
 });
 
 // Mixitup - For Filtering
 var containerEl = document.querySelector('.gallery');
 /* var mixer = mixitup(containerEl);
 var containerEl = document.querySelector('.gallery'); */
-var mixer = mixitup(containerEl, {  //For Conflict from Tab Pane
-  selectors: {
-    control: '[mix-control]', //For Conflict from Tab Pane
-  }
+var mixer = mixitup(containerEl, { //For Conflict from Tab Pane
+    selectors: {
+        control: '[mix-control]', //For Conflict from Tab Pane
+    }
+});
+
+// For Owl Carousel Slider
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        autoplay: true,
+        autoplayTimeout: 200,
+        smartSpeed: 300,
+        loop: true,
+        responsive:{
+          0:{
+              items:1
+          },
+          600:{
+              items:3
+          },
+          1000:{
+              items:5
+          }
+      }
+
+    });
 });
